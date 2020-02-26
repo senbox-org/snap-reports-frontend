c3c70a1640669d61fbad9a6fc8553231222928ea
Mandarancio <manda.mgf@gmail.com>
2020-02-26 17:22:49 +0100

    removed old code

--- src/components/JobTable.vue
+++ src/components/JobTable.vue
@@ -8,40 +8,10 @@
       @click="rowClicked"
     >
     </b-table>
-    <!-- <table>
-      <thead>
-        <tr>
-          <th> ID </th>
-          <th> Job Number </th>
-          <th> Branch </th>
-          <th> Tag </th>
-          <th> Time stamp </th>
-          <th> Duration </th>
-          <th> Result </th>
-        </tr>
-      </thead>
-      <tr v-for="(job, index) in jobs.jobs" :key="index" :class="[job.result.tag == 'SUCCESS' ? 'passed' : 'failed' ]">
-        <td><router-link :to="`/job/${job.ID}`">{{ job.ID }}</router-link></td>
-        <td> {{ job.jobnum }} </td>
-        <td> {{ job.dockerTag.name }} </td>
-        <td> {{ job.testScope }} </td>
-        <td> {{ job.timestamp_start }} </td>
-        <td> {{ duration(job) }} </td>
-        <td> {{ job.result.tag }} </td>
-      </tr>
-    </table> -->
   </div>
 </template>
 <script>
 import router from '../router'
-
-const format = require('string-format');
-
-function i2s(num) {
-  if (num < 10)
-    return "0"+num;
-  return num;
-}
 
 export default {
   name: 'Overview',
@@ -85,15 +55,6 @@ export default {
     jobs: Object
   },
   methods: {
-    duration(job) {
-      var end = new Date(job.timestamp_end);
-      var start = new Date(job.timestamp_start);
-      console.log(end.getTime())
-      var diff = end.getTime() - start.getTime();
-      var hour = Math.floor(diff / 3.6e6);
-      var min = Math.round(diff / 6e4)  - hour * 60;
-      return format("{0}h{1}m", hour, i2s(min));
-    },
     rowClicked(record) {
       router.push("/job/"+record["ID"]);
     }
