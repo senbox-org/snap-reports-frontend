c4f3c7c774121bb9a3560cafc6652449d2e9c6e9
Martino Ferrari <manda.mgf@gmail.com>
2020-03-05 10:50:22 +0100

    using local hostname;

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,5 +1,5 @@
 export default {
-  service: 'http://snap-build-server.tilaa.cloud:9090/',
+  service: 'http://snap-report-be:9090/',
   call() {
     let args = Array.prototype.slice.call(arguments, 0);
     return this.service+args.join('/')
