ef88fb2c23499603f4f958c97fad0e433d9136ee
Mandarancio <manda.mgf@gmail.com>
2020-03-06 10:46:07 +0100

    using config

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,5 +1,7 @@
+const config = require('config');
+
 export default {
-  service: 'http://snap-build-server.tilaa.cloud:9090/',
+  service: 'http://'+config.get('service.host')+':'+config.get('service.port'),
   call() {
     let args = Array.prototype.slice.call(arguments, 0);
     return this.service+args.join('/')
