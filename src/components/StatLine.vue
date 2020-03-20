45f1286f6510ea414b021815b61d4847a812572c
Martino Ferrari <manda.mgf@gmail.com>
2020-03-20 20:21:27 +0100

    added warning part

--- src/components/StatLine.vue
+++ src/components/StatLine.vue
@@ -1,9 +1,11 @@
 <template>
   <g>
     <rect x="0" y="5" :width="passed_perc()" height="20" class="success"/>
-    <rect :x="passed_perc()" y="5" :width="failed_perc()" height="20" class="failed"/>
-    <text  v-if="passed > 0" x="1%" y="19" class="inner">{{passed}}</text>
-    <text v-if="(total - passed) > 0" x="99%" y="19" class="inner" text-anchor="end">{{total - passed}}</text>
+    <rect :x="passed_perc()" y="5" :width="warning_perc()" height="20" class="none"/>
+    <rect :x="failed_begin()" y="5" :width="failed_perc()" height="20" class="failed"/>
+    <text v-if="passed > 0" x="1%" y="19" class="inner">{{passed}}</text>
+    <text v-if="warning > 0" :x="warn_pos()" y="19" class="inner">{{warning}}</text>
+    <text v-if="failed()> 0" x="99%" y="19" class="inner" text-anchor="end">{{failed()}}</text>
     <text x="0" y="40">Total: {{total}}</text>
   </g>
 </template>
@@ -13,19 +15,38 @@ export default {
   props:{
     total: Number,
     passed: Number,
+    warning: Number
   },
   methods: {
-    passed_perc: function() {
+    passed_perc() {
       return Math.round(this.passed/this.total * 100)+"%";
     },
-    failed_perc: function() {
-      return (100 - Math.round(this.passed/this.total * 100)) + "%";
+    failed_perc() {
+      return Math.round(this.failed()/this.total * 100) +"%"
     },
-    failednum_pos: function() {
-      return Math.round(this.passed/this.total * 100 + 1)+"%";
+    failed_begin() {
+      return Math.floor((this.total-this.failed())/this.total * 100) + "%";
+    },
+    warning_perc() {
+      if (this.warning == undefined)
+        return 0;
+      return Math.round(this.warning/this.total *100) + '%';
+    },
+    warn_pos() {
+      if (this.passed + this.warning == this.total) {
+        return "99%";
+      }
+      var x =this.passed + this.warning / 2;
+      return Math.round(x/this.total * 100) + "%";
+    },
+    failed() {
+      var num = this.total - this.passed;
+      if (this.warning != undefined)
+        num -= this.warning;
+      return num;
     }
   }
-}
+};
 </script>
 <style>
 polygon {
@@ -55,6 +76,9 @@ label {
 
 .failed {
   fill: rgb(205, 92, 92);
+}
+.none {
+  fill: #999;
 }
 .inner {
   fill: #fff;
