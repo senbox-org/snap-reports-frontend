f7a9d8d21b5c18acb27bcb3a015677d2afadcfbf
BALDO Frederick <frederick.baldo@csgroup.eu>
2023-10-23 11:27:47 +0200

    Fix compare page

--- src/views/branches/Compare.vue
+++ src/views/branches/Compare.vue
@@ -20,7 +20,7 @@
 
           <b-button @click="add_filter()">+</b-button>
           
-          <span  v-for="(filter, index) in filters" :key="index">
+          <span v-for="(filter, index) in filters" :key="index">
             <span class="spacer"/>
             <b-tag type="is-primary" size="is-medium" @close="removeFilter(index)" closable>
               {{filter.name}} : {{filter.value}}
@@ -28,36 +28,36 @@
           </span>
         </div>
 
-      <b-table
-      :data="value"
-      :default-sort="['test_ID', 'asc']"
-      @click="(row) => go_to_test(row.test_ID)"
-      hoverable>
-        <template slot-scope="props">
-          <b-table-column field="test_ID" label="ID" sortable numeric>
+        <b-table 
+        :data="data"
+        :loading="loading"
+        :default-sort="['test_ID', 'asc']"
+        paginated
+        @click="(row) => go_to_test(row?.test_ID)"
+        hoverable>
+          <b-table-column field="test_ID" label="ID" sortable numeric v-slot="props">
             {{ props.row.test_ID}}
           </b-table-column>
-          <b-table-column field="test_name" label="Name" sortable>
+          <b-table-column field="test_name" label="Name" sortable v-slot="props">
             {{ props.row.test_name}}<br>
           </b-table-column>
-          <b-table-column field="br_a_avg" :label="tag_a" sortable numeric>
+          <b-table-column field="br_a_avg" :label="tag_a" sortable numeric v-slot="props">
             {{ props.row.br_a_avg}} {{units[field]}}<br>
             <span class="lighttext">N. of executions: {{props.row.br_a_count}}</span>
           </b-table-column>
-          <b-table-column field="br_b_avg" :label="tag_b" sortable numeric>
+          <b-table-column field="br_b_avg" :label="tag_b" sortable numeric v-slot="props">
             {{ props.row.br_b_avg}} {{units[field]}}<br>
             <span class="lighttext">N. of executions: {{props.row.br_b_count}}</span>
           </b-table-column>
-          <b-table-column field="diff_abs" label="Absolute difference" sortable numeric>
+          <b-table-column field="diff_abs" label="Absolute difference" sortable numeric v-slot="props">
             {{ props.row.diff_abs.toFixed(1)}} {{units[field]}}<br>
           </b-table-column>
-          <b-table-column field="diff_rel" label="Relative difference" sortable numeric>
+          <b-table-column field="diff_rel" label="Relative difference" sortable numeric v-slot="props">
             {{ props.row.diff_rel.toFixed(1)}}%<br>
           </b-table-column>
-          <b-table-column field="author" label="Author" sortable>
+          <b-table-column field="author" label="Author" sortable v-slot="props">
             {{ props.row.author}}<br>
           </b-table-column>
-        </template>
       </b-table>
     </article>
   </div>
@@ -72,65 +72,92 @@
   import api from '@/assets/api.js';
 
   export default {
-    data() {
-      return {
-        tag_a: this.$route.params.tag_a,
-        tag_b: this.$route.params.tag_b,
-        addFilterActive: false,
-        field: 'duration',
-        units: {
-          duration: 's',
-          cpu_time: 's',
-          memory_avg: 'MB',
-          memory_max: 'MB',
-          io_write: '',
-          io_read: '',
-        },
-        value: [],
-        filters: [],
-      }
+    //   value: [],
+      data() {
+        return {
+            tag_a: this.$route.params.tag_a,
+            tag_b: this.$route.params.tag_b,
+            addFilterActive: false,
+            field: 'duration',
+            units: {
+                duration: 's',
+                cpu_time: 's',
+                memory_avg: 'MB',
+                memory_max: 'MB',
+                io_write: '',
+                io_read: '',
+            },
+            data: [],
+            filters: [],
+            loading: false
+        };
     },
     mounted() {
-      axios
-        .get(api.call('api/branch/compare', this.tag_a, this.tag_b, this.field))
-        .then((res) => (this.value = res['data']));
+        this.loading = true;
+        axios
+            .get(api.call('api/branch/compare', this.formatField(this.tag_a), this.formatField(this.tag_b), this.field))
+            .then(({data}) => {
+                this.data = data;
+                this.loading = false;
+            })
+            .catch((err) => {
+                this.data = [];
+                this.loading = false;
+                throw err;
+            }) 
     },
     components: {
-
     },
     methods: {
-      go_to_test(test){
-        router.push("/test/"+test);
-      },
+        formatField(fieldName) {
+            if (!fieldName.startsWith('snap:')) {
+                return 'snap:' + fieldName;
+            }
+            return fieldName;
+        },
+        compare() {
+            axios
+                .get(api.call('api/branch/compare', this.formatField(this.tag_a), this.formatField(this.tag_b), this.field))
+                .then((res) => {
+                    this.data = [...res.data];
+                    console.log('mounted', res.data);
+                }); 
+        },
+        go_to_test(test){
+            router.push("/test/"+test);
+        },
       update(){
         let fields_var = this.field;
-        if (this.filters.length) {
-          fields_var += '?';
-        }
+            if (this.filters.length) {
+            fields_var += '?';
+            }
         for (let i=0; i < this.filters.length; i++) {
-          if (i > 0) {
-            fields_var += '&';
-          }
-          let filter = this.filters[i];
-          fields_var += filter.name + '=' + filter.value;
+            if (i > 0) {
+                fields_var += '&';
+            }
+        let filter = this.filters[i];
+        fields_var += filter.name + '=' + filter.value;
         }
         axios
-        .get(api.call('api/branch/compare', this.tag_a, this.tag_b, fields_var))
-        .then((res) => (this.value = res['data']));  
+            .get(api.call('api/branch/compare', this.formatField(this.tag_a), this.formatField(this.tag_b), fields_var))
+            .then((res) => {
+                this.data = [...res.data];
+                console.log(res);
+            });  
       },
       add_filter(){
         this.$buefy.modal.open({
-                    parent: this,
-                    component: FilterForm,
-                    hasModalCard: true,
-                    trapFocus: true,
-                    events: {
-                      'addFilterEvent': value => {
-                        this.filters.push(value)
-                        this.update()
-                      }
-                    }
-              })
+            parent: this,
+            component: FilterForm,
+            hasModalCard: true,
+            trapFocus: true,
+            events: {
+                'addFilterEvent': value => {
+                    this.filters.push(value)
+                    this.update()
+                }
+            }
+        })
       },
       select_branch() {
         this.$buefy.modal.open({
@@ -140,7 +167,7 @@
             trapFocus: true,
             events: {
               'selectBranchEvent': value => {
-                let url = "/branch/compare/"+this.tag_a+'/'+value;
+                let url = `/branch/compare/${this.formatField(this.tag_a)}/${this.formatField(value)}`;
                 router.replace({path: url});
                 this.tag_b = value;
                 this.update();
