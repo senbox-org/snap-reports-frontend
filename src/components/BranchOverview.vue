0cda302cb5514c8fb68e9d3089a66cd08920da83
BALDO Frederick <frederick.baldo@csgroup.eu>
2024-01-11 16:14:46 +0100

    fix: Wait async data before render in Branch vues

--- src/components/BranchOverview.vue
+++ src/components/BranchOverview.vue
@@ -8,27 +8,25 @@
       </b-select>
 
     </b-field>
-    <b-table :data="rows">
-      <template slot-scope="props" v-if="props.row">
-        <b-table-column field="title" label="">
+    <b-table :data="rows" :loading="!data">
+        <b-table-column v-slot="props" field="title" label="">
           <b>{{ props.row.title}}</b>
         </b-table-column>
-        <b-table-column field="cpu" label="CPU Time" numeric>
+        <b-table-column v-slot="props" field="cpu" label="CPU Time" numeric>
           <b-tag :type="props.row.status('cpu')">
             {{ props.row.cpu() }}
           </b-tag>
         </b-table-column>
-        <b-table-column field="memory" label="Average Memory" numeric>
+        <b-table-column v-slot="props" field="memory" label="Average Memory" numeric>
           <b-tag :type="props.row.status('memory')">
             {{ props.row.memory() }}
           </b-tag>
         </b-table-column>
-        <b-table-column field="read" label="IO Read" numeric>
+        <b-table-column v-slot="props" field="read" label="IO Read" numeric>
           <b-tag :type="props.row.status('read')">
             {{ props.row.read() }}
           </b-tag>
         </b-table-column>
-      </template>
     </b-table>
   </section>
 </template>
@@ -45,7 +43,34 @@
     data() {
       return {
         data: undefined,
-        rows: [
+        rows: [],
+        sel: '10'
+      }
+    },
+    methods: {
+      total(field) {
+        const reducer = (acc, val) => acc + val['res_'+field];
+        return this.data.reduce(reducer, 0);
+      },
+
+      ref(field) {
+        const reducer = (acc, val) => acc + val['ref_'+field];
+        return this.data.reduce(reducer, 0);
+      },
+      improved(field) {
+        const reducer = (acc, val) => acc + (val['res_'+field] < val['ref_'+field] ? 1 : 0);
+        return this.data.reduce(reducer, 0);
+      },
+      update() {
+        axios
+        .get(api.call('api/branch', this.branch, 'details', this.sel))
+        .then((res) => {
+            this.data = res.data.details;
+            this.setRows();
+        });
+      },
+      setRows() {
+        this.rows = [
           {
             title: 'Total',
             cpu: () => (this.total('cpu').toFixed(1) + ' s'),
@@ -74,28 +99,7 @@
             read: () => (this.improved('read') + '/' + this.data.length),
             status: () => undefined
           }
-        ],
-        sel: '10'
-      }
-    },
-    methods: {
-      total(field) {
-        const reducer = (acc, val) => acc + val['res_'+field];
-        return this.data.reduce(reducer, 0);
-      },
-
-      ref(field) {
-        const reducer = (acc, val) => acc + val['ref_'+field];
-        return this.data.reduce(reducer, 0);
-      },
-      improved(field) {
-        const reducer = (acc, val) => acc + (val['res_'+field] < val['ref_'+field] ? 1 : 0);
-        return this.data.reduce(reducer, 0);
-      },
-      update() {
-        axios
-        .get(api.call('api/branch', this.branch, 'details', this.sel))
-        .then((res) => (this.data = res.data.details));
+        ];
       }
     }, 
     mounted() {
